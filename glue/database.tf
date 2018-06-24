409d1f67256aaa67241e4acea7a5ec610030842c
Daniel Siguero <daniel.siguero@gmail.com>
2018-06-24 02:52:25 +0100

    - Add Glue DB resource

---
+++ glue/database.tf
@@ -0,0 +1,3 @@
+resource "aws_glue_catalog_database" "ingest" {
+  name = "aws-glue-ingest-medium"
+}
--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ module "prerequisites-glue" {
   environment                 = "${var.environment}"
   aws_glue_source_bucket_name = "aws-glue-medium-post"
 }
+
+module "glue" {
+  source     = "./glue"
+
+  iam_glue_role = "${module.prerequisites-glue.iam_glue_role_arn}"
+}
+
